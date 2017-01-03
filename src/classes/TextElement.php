8522a9073206094bec9c001f329edcb25a03f97b
gsachot <gsachot@matlo.com>
2017-01-03 12:07:08 +0100

    second commit

--- src/classes/TextElement.php
+++ src/classes/TextElement.php
@@ -1,76 +1,16 @@
 <?php
 
+require_once 'HtmlElement.php';
+
 /**
 *
 */
-class HtmlElement {
+class TextElement {
+
   private
-    $name,
-    $selfClose,
-    $children,
-    $properties;
-
-  function __construct(String $name, Bool $selfClose = false) {
-    $this->name = $name;
-    $this->selfClose = $selfClose;
-    $this->children = array();
-    $this->properties = array();
-  }
-
-  public function getName() {
-    return $this->name;
-  }
-
-  public function addChild(HtmlElement $child) {
-    array_push($this->children, $child);
-    return $this;
-  }
-
-  public function addText(String $child) {
-    array_push($this->children, $child);
-    return $this;
-  }
-
-  public function addProperty(String $key,String $value) {
-    $this->properties[$key] = $value;
-    return $this;
-  }
-
-  public function render() {
-    $html = '<'.$this->name;
-
-    foreach ($this->properties as $key => $value) {
-      $html .= ' '.$key.'="'.$value.'"';
-    }
-
-    if ($this->selfClose) {
-      $html .= '/>';
-    }
-    else {
-      $html .= '>';
-
-      foreach ($this->children as $child) {
-        if (gettype($child) == 'object')
-          $html .= $child->render();
-        else
-          $html .= $child;
-      }
-
-     $html .= '</'.$this->name.'>';
-    }
-
-    return $html;
-  }
-}
-
-/**
-*
-*/
-class TextElement
-{
-  private $id;
-  private $label;
-  private $type;
+   $id,
+   $label,
+   $type;
 
   function __construct(String $id, String $label, String $type = 'text') {
     $this->id = $id;
@@ -89,6 +29,22 @@ class TextElement
   public function render() {
     return $this->htmlElement()->render();
   }
+}
+
+class SelectElement {
+
+  private
+    $id,
+    $option;
+
+  function __construct(String $id, Srting $option) {
+    $this->$id = $id;
+    $this->$option = $option;
+  }
+
+    public function htmlSelect
+
+
 }
 
 ?>
